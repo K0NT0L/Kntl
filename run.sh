f788e62b1264ab309579e0bd237e59602606e4de
K0NT0L <46758245+K0NT0L@users.noreply.github.com>
2019-01-17 20:00:10 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/system/xbin/bash
+ 
+ # Variables
+ 
+ $blue='\e[1;34m'
+$cyan='\e[1;36m'
+$red='\e[1;31m'
+ $bersih ="clear'
+ 
+ clear 
+ 
+ echo "$cyanSedang Proses Penginstallan eaa" 
+ echo "Waiting ea gan :v.."
+ 
+ sleep 2
+ 
+apt update && apt upgrade
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+
+clear
+
+clear
+echo "Waiting..... "
+echo ""
+sleep 1
+sh fuck.sh
+
+
