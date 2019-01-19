73fdba2ea619a72905684f3592c62890229e63e5
K0NT0L <46758245+K0NT0L@users.noreply.github.com>
2019-01-19 12:13:09 +0700

    Create knt.php

---
+++ knt.php
@@ -0,0 +1,22 @@
+ #!/data/data/com.termux/files/usr/bin/php
+<?php
+if(strtolower(substr(PHP_OS, 0, 3)) == "win") {
+$bersih="clear";
+} else {
+$bersih="clear";
+}
+
+$red = "\e[91m";
+$yellow = "\e[93m";
+$blue = "\e[36m";
+
+echo "\n$yellow
+  ________________________
+<<<<  Author : StarFuckTak>>>>
+  ------------------------\n";
+  echo "\n";
+ echo "\n$green==>>Trima Kasih Sudah Datang :v\n";
+  echo "\n$red==>>Good Bye Script Kiddie😇\n";
+  echo "\n$blue==>>Please Don't Forget To Support Us\n";
+echo "\n";
+?>
